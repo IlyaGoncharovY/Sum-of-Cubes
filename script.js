2aa26fde8f35c9d06f8bcb0e309504d622e3488d
Ilya <goncharov.i@rambler.ru>
2021-12-26 16:59:01 +0300

    instruction

---
+++ script.js
@@ -0,0 +1,6 @@
+function sumCubes(n){
+    // Write a function that takes a positive integer n, 
+    // sums all the cubed values from 1 to n, and returns that sum.
+    // Assume that the input n will always be a positive integer.
+  }
+  
